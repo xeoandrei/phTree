61bfbe0317d2a45e898d24ca0cfcc30d7181ed58
Francis Jacob Taiño <francistaino16@gmail.com>
2022-12-09 01:56:53 +0800

    added confirmation for upgrading and downgrading users.

--- admin-view-users.php
+++ admin-view-users.php
@@ -27,9 +27,9 @@
                     <td><?php echo $val -> usertype ?></td>
                     <td><?php echo $val -> email ?></td>
                     <?php if($val -> usertype == 'admin'){ ?>
-                        <td><a class='btn btn-danger' href="controller/downgrade-account_query.php?userId=<?php echo $val -> userId ?>">Downgrade</a></td>
+                        <td><a class='btn btn-danger' href="controller/downgrade-account_query.php?userId=<?php echo $val -> userId ?>" onclick="return confirm('Are you sure you want to downgrade this user to user level?')">Downgrade</a></td>
                     <?php } else { ?>
-                        <td><a class='btn btn-success' href="controller/upgrade-account_query.php?userId=<?php echo $val -> userId ?>">Upgrade</a></td>
+                        <td><a class='btn btn-success' href="controller/upgrade-account_query.php?userId=<?php echo $val -> userId ?>" onclick="return confirm('Are you sure you want to upgrade this user to admin level?')">Upgrade</a></td>
                     <?php } ?>
 
                 </tr>
