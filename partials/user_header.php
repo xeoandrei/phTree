41a89c25959223092876c2e7cc05c0e2740fa427
Andrei Delgado <xeoandrei@gmail.com>
2022-12-08 18:27:43 +0800

    added user view for appli & furni in header&footer

--- partials/user_header.php
+++ partials/user_header.php
@@ -28,10 +28,10 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" href="#">Appliances</a>
+          <a class="nav-link" href="user-view-appliances.php">Appliances</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Furnitures</a>
+          <a class="nav-link" href="user-view-furniture.php">Furnitures</a>
         </li>
       </ul>
 
