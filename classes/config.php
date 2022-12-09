78575102d86723c0b06984ff1accff2c2fb5f56f
Andrei Delgado <xeoandrei@gmail.com>
2022-12-09 21:19:17 +0800

    this

--- classes/config.php
+++ classes/config.php
@@ -1,7 +1,11 @@
 <?php
     session_start();
-    define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASSWORD', '');
-    define('DB_NAME', 'appliance_furniture');
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    define('DB_HOST', $cleardb_url["host"]);
+    define('DB_USER', $cleardb_url["user"]);
+    define('DB_PASSWORD', $cleardb_url["pass"]);
+    define('DB_NAME', substr($cleardb_url["path"],1));
+
+    $active_group = 'default';
+    $query_builder = TRUE;
 ?>
